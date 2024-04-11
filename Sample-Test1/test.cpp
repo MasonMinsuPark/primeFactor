e0c2d022130e1c6d29f4ed200a2ec6da89e8573c
minsu <ms5917.park@samsung.com>
2024-04-11 11:18:59 +0900

    test2

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,14 @@
 #include "pch.h"
 #include "../primeFactors/PrimeFactor.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, TestName1) {
 	PrimeFactor primeFactor;
 	vector<int> expectedResult = {};
 	EXPECT_EQ(primeFactor.getPrime(1), expectedResult);
+}
+
+TEST(TestCaseName, TestName2) {
+	PrimeFactor primeFactor;
+	vector<int> expectedResult = {2};
+	EXPECT_EQ(primeFactor.getPrime(2), expectedResult);
 }
--- primeFactors/PrimeFactor.cpp
+++ primeFactors/PrimeFactor.cpp
@@ -6,6 +6,8 @@ class PrimeFactor {
 public:
 	vector<int> getPrime(int n) {
 		vector<int> returnVector;
+		if (n == 2)
+			returnVector.push_back(2);
 		return returnVector;
 	}
 };
