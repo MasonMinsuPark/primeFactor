db58f334869303125e15c5cd0089a9a58c664226
minsu <ms5917.park@samsung.com>
2024-04-11 11:13:50 +0900

    test1

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,6 +3,6 @@
 
 TEST(TestCaseName, TestName) {
 	PrimeFactor primeFactor;
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	vector<int> expectedResult = {};
+	EXPECT_EQ(primeFactor.getPrime(1), expectedResult);
 }
--- primeFactors/PrimeFactor.cpp
+++ primeFactors/PrimeFactor.cpp
@@ -1,3 +1,11 @@
+#include <vector>
+
+using namespace std;
+
 class PrimeFactor {
 public:
+	vector<int> getPrime(int n) {
+		vector<int> returnVector;
+		return returnVector;
+	}
 };
